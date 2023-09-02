96b78bea7444f9a9019b9b635d2df8f6957b58bc
Minhaj Sorder <74466041+minhaj57sorder@users.noreply.github.com>
2023-09-02 18:09:13 +0600

    api example

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,8 +1,10 @@
 import { defineStore } from "pinia";
+import { api } from "src/boot/axios";
 import { ref } from "vue";
 
 export const useCounterStore = defineStore("counter store", () => {
   const count = ref(0);
+  const hadithList = ref(null);
   const add = () => {
     count.value = count.value + 1;
   };
@@ -17,6 +19,22 @@ export const useCounterStore = defineStore("counter store", () => {
       return;
     }
   };
+  const getHadith = async () => {
+    const config = {
+      method: 'get',
+      url: '/api/hadiths',
+      headers: {
+        "Authorization": "Bearer token",
+        "Content-Type": "application/json"
+      }
+    }
+    try {
+      let response = await api.request(config)
+      hadithList.value = response.data
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return {
     add,
     subtruct,
